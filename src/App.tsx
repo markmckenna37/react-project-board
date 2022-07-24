505bb648652d4eeeaeffed7577ee04d0a466bc74
Mark <youremail@gmail.comg>
2022-07-24 14:59:32 -0600

    added initial state, mapped columns

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,45 @@ import React from 'react';
 import Column from "./components/Column"
 import './App.css';
 
+const initState:any = [
+  {
+    title: "TODO",
+    todos: [],
+    input: ""
+  },
+  {
+    title: "IN PROGRESS",
+    todos: [],
+    input: ""
+  },
+  {
+    title: "COMPLETE",
+    todos: [],
+    input: ""
+  }
+]
+
+
 function App() {
+
+  const [col] = React.useState(initState)
+  //TODO:
+  //Method for handling input changes on add-card
+
+  //Method for handling the addition of an add card
+
+  //method for handling the card shifts between columns
   return (
     <div className="wrapper">
       <div className="mckello">
         <h1>McKello</h1>
         <div className="columnContainer">
-          <Column 
-            label="To Do"
-            btnLabel="Move to 'In Progress'"
-          />
-          <Column 
-            label="In Progress"
-            btnLabel="Move to 'doing'"
-          />
-          <Column 
-            label="Done"
-            
-          />
+          {col.map(() => (
+            <Column 
+              label={"todo"}
+              btnLabel="Move to 'In Progress'"
+            />
+          ))}
 
         </div>
       </div>
